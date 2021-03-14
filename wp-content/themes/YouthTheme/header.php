b683a3dd2c8677d8d6f88a72a7781493aa414033
Exequiel Beker <exequielbeker@gmail.com>
2021-03-14 16:05:08 -0300

    Added header

--- wp-content/themes/YouthTheme/header.php
+++ wp-content/themes/YouthTheme/header.php
@@ -12,48 +12,50 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'youth-theme' ); ?></a>
+    <?php wp_body_open(); ?>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$youth_theme_description = get_bloginfo( 'description', 'display' );
-			if ( $youth_theme_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $youth_theme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+    <!-- Header and navigation -->
+    <header id="masthead">
+        <!--Navigation -->
+        <nav class="navbar navbar-light bg-light">
+            <!-- Page branding -->
+            <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                <?php the_custom_logo();?>
+                <img src="https://via.placeholder.com/30x30" class="d-inline-block align-top" alt="" loading="lazy">
+                <?php _e( 'Red Interamericana de Jovenes', 'youth' )?>
+            </a> <!-- Page branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'youth-theme' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+            <!-- page navigation -->
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+				<?php wp_nav_menu( 
+                              array( 
+                              'theme_location' => 'primary_navbar',
+                              'depth' => 2,
+                              'container' => 'div',
+                              'container_id' => 'navbarNav',
+                              'container_class' => 'collapse navbar-collapse',
+                              'menu_class' => 'navbar-nav align-items-baseline',
+                              'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+                              'walker'=> new WP_Bootstrap_Navwalker(),)
+                );?>
+            </nav>
+
+        </nav>
+
+
+    </header> <!-- #masthead -->
